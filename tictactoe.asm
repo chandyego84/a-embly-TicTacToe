6fcd23ffa22f61979cce9a9b3799e6ac43a2f2a3
emma <97130396+emmaebjohnson@users.noreply.github.com>
2023-02-18 19:08:11 -0800

    until check win

--- tictactoe.asm
+++ tictactoe.asm
@@ -7,6 +7,9 @@ gameboard: .asciiz "0|1|2\n3|4|5\n6|7|8\n" #gameboard slay but chandy will make 
 pickmove: .asciiz "where on the board do you want to go? (enter a value 0-8)\n" #pick a move msg
 confirm: .asciiz "your move has been recorded\n"
 
+P1Wins: .asciiz "you did it dumb slut, aka Player 1"
+P2Wins: .asciiz "you did it dumb slut, aka Player 2"
+
 board: .word 0, 0, 0, 0, 0, 0, 0, 0, 0 #current array, named board >:)
 empty: .word 0
 
@@ -19,7 +22,7 @@ main: #main
  ###initalizing most likely
  la $s0, board #setting the starting address of the board to $s0
  li $s2, 0 # HERE IS INDEX
-
+ li $s4, 0
 ### PRINTING INDEX
  li $v0, 1
  move $a0, $s2
@@ -74,8 +77,9 @@ P1:
   li $v0, 4
   la $a0, confirm 
   syscall
-  
-  jal P2Print
+
+  jal H1
+
 
 P2:
  bne $t3, 0, P2Print
@@ -84,9 +88,39 @@ P2:
   li $v0, 4
   la $a0, confirm 
   syscall
+  jal H1
+  
 
-  jal P1Print
+H1:
+  li $s2, 0
 
+  sll $t4, $s2, 2 # $t4 = i * 4;
+  add $t4, $t4, $s0 # $t4 = i * 4 + $s0
+  lw $t5, 0($s0) # $t0 = A[i]
+
+  sll $t4, $s2, 2 # $t4 = i * 4;
+  add $t4, $t4, $s0 # $t4 = i * 4 + $s0
+  lw $t5, 0($s0) # $t0 = A[i]
+
+  sll $t4, $s2, 2 # $t4 = i * 4;
+  add $t4, $t4, $s0 # $t4 = i * 4 + $s0
+  lw $t5, 0($s0) # $t0 = A[i]
+
+  add $s4, $s4, $t5 
+
+  addi $s2, $s2, 1 # i = i + 1
+  bne $s2, 3, H1 # if (i != $s1), go back to loop
+
+addi $s2, $s2, -3
+
+beq $s4, 3, P1Wins
+beq $s4, 6, P2Wins
+
+bne $s2, 9, H1 # if (i != $s1), go back to loop
+
+where:
+ bne $s3, 1, P2Print #which player is it?
+ jal P1Print
 
  #exit
  li $v0, 10
